17fb32631e6524d8b4a8ab84334bcd04824653e0
thedavibur <44172388+thedavibur@users.noreply.github.com>
2019-02-06 13:56:16 +0100

    Add files via upload

---
+++ exameJS13/exame13.js
@@ -0,0 +1,36 @@
+var forma = prompt('que queres traducir? 1.- ingles-galego 2.- galego-ingles');
+
+
+
+
+
+switch(forma)
+{
+  case '1':
+  var respuesta1 = prompt('traduce table');
+  var resultado = 'mesa';
+  while (respuesta1 != resultado){
+  	if (respuesta1 = resultado)
+	{
+		alert('MOI BEN')
+	}
+	else {
+		alert('MOI MAL')
+	}
+  }
+  break;
+
+  case '2':
+  var respuesta1 = prompt('traduce mesa')
+  var resultado = 'table';
+  while (respuesta1 != resultado){
+  	if (respuesta1 = resultado)
+	{
+		alert('MOI BEN')
+	}
+	else {
+		alert('MOI MAL')
+	}
+  }
+
+    break;
